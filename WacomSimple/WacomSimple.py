07c3a9baee492c8a2f8fb52443d71a0a2ba3541d
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2015-10-09 10:23:00 +0200

    Converting Wacom example to PyObjC.

---
+++ WacomSimple/WacomSimple.py
@@ -0,0 +1,28 @@
+from AppKit import *
+
+class PressureWinController(NSObject):
+
+
+    def awakeFromNib(self):
+        pass
+
+    def opacityMenuAction_(sender):
+        pass
+
+    def lineSizeMenuAction_(sender):
+        pass
+
+    def captureMouseMovesAction_(sender):
+        pass
+
+    def updateStatsDuringDragAction_(sender):
+        pass
+
+    def openColorPanel_(sender):
+        pass
+
+    def changeColor_(sender):
+        pass
+
+    def wtvUpdatedStats_(sender):
+        pass
