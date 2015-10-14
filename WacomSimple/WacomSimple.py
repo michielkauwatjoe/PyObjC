00f0d76ab2cf944e4e12d3825303d4769c5bf66a
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2015-10-14 11:42:52 +0200

    Extending interface for Wacom simple example

--- WacomSimple/WacomSimple.py
+++ WacomSimple/WacomSimple.py
@@ -1,27 +1,83 @@
 from AppKit import *
 
+class WTView(NSView):
+    u"""
+    """
+
+    mEventType = None
+    mMouseX = None
+    mMouseY = None
+    mSubX = None
+    mSubY = None
+    mPressure = None
+    mForeColor = None
+    mAdjustOpacity = None
+    mAdjustSize = None
+    mCaptureMouseMoves = None
+    mUpdateStatsDuringDrag = None
+
+    def setForeColor_(self, newColor):
+        pass
+
+    def setAdjustOpacity_(self, adjust):
+        pass
+
+    def setAdjustSize_(self, adjust):
+        pass
+
+    def setCaptureMouseMove_(self, value):
+        pass
+
+    def setUpdateStatsDuringDrag_(self, value):
+        pass
+
+    def handleMouseEvent(self, theEvent):
+        pass
+
+    def drawCurrentDataFromEvent_(self, theEvent):
+        pass
+
 class PressureWinController(NSObject):
+    u"""
+    """
+
+    txtEventType = IBOutlet()
+    txtMouseX = IBOutlet()
+    txtMouseY = IBOutlet()
+    txtPressure = IBOutlet()
+    clrForeColor = IBOutlet()
+    wtvTabletDraw = IBOutlet()
+    mnuLineSize = IBOutlet()
+    mnuOpacity = IBOutlet()
+    mnuCaptureMouseMoves = IBOutlet()
+    mnuUpdateStatsDuringDrag = IBOutlet()
 
     def awakeFromNib(self):
         pass
 
-    def opacityMenuAction_(sender):
+
+    @IBAction
+    def opacityMenuAction_(self, sender):
         pass
 
-    def lineSizeMenuAction_(sender):
+    @IBAction
+    def lineSizeMenuAction_(self, sender):
         pass
 
-    def captureMouseMovesAction_(sender):
+    @IBAction
+    def captureMouseMovesAction_(self, sender):
         pass
 
-    def updateStatsDuringDragAction_(sender):
+    @IBAction
+    def updateStatsDuringDragAction_(self, sender):
         pass
 
-    def openColorPanel_(sender):
+    @IBAction
+    def openColorPanel_(self, sender):
         pass
 
-    def changeColor_(sender):
+    def changeColor_(self, sender):
         pass
 
-    def wtvUpdatedStats_(sender):
+    def wtvUpdatedStats_(self, theNotification):
         pass
